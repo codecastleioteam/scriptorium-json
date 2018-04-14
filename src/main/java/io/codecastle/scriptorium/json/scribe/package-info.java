eab1d2ca29a280d766292750322f44346b9dd3ca
Doug Valenta <dougvalenta@gmail.com>
2018-04-14 08:26:20 -0700

    Adding package and overview doc comments

---
+++ src/main/java/io/codecastle/scriptorium/json/scribe/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2018 Doug Valenta.
+ * Licensed under the terms of the MIT License.
+ */
+
+/**
+ * Classes for outputting JSON formatted text in an orderly manner, including helpers for
+ * rendering JSON tokens and numbers, and escaping string literals.
+ */
+package io.codecastle.scriptorium.json.scribe;
